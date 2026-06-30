f153ce99f6432424b475cbde6cfe2316993f1a27
Mango-The-Froot <mangothefroot14@gmail.com>
2026-06-30 02:32:33 -0500

    Updated Rat Script

--- rat.gd
+++ rat.gd
@@ -2,8 +2,10 @@ extends CharacterBody2D
 
 class_name RatEnemy
 
-const speed = 10
-var chasing: bool
+@onready var sprite = $AnimatedSprite2D
+
+const speed = 100
+var chasing: bool = false
 
 var health = 20
 var healthMax = 20
@@ -19,9 +21,31 @@ const gravity = 20
 var kBForce = 100
 var roaming: bool = true
 
+func _process(delta):
+	sprite.play("default")
+	if velocity.x > 0:
+		sprite.flip_h = true
+	if velocity.x < 0:
+		sprite.flip_h = false
+	if !is_on_floor():
+		velocity.y += gravity
+		velocity.x = 0
+	move(delta)
+	move_and_slide()
+	
+func move(delta):
+	if !dead:
+		if !chasing:
+			velocity += dir * speed * delta
+		roaming = true
+	elif dead:
+		velocity.x = 0
 
-func _on_direction_timeout() -> void:
+func _on_direction_timeout():
 	$Direction.wait_time = choose([1.5,2.0,2.5])
+	if !chasing:
+		dir = choose([Vector2.RIGHT, Vector2.LEFT])
+		velocity.x = 0
 	
 func choose(array):
 	array.shuffle()
