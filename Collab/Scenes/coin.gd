18db34ea291dd875ca077167e5ee7ff0ce117e2e
v1Rai <dragonraidrake@gmail.com>
2026-07-01 10:08:44 +0900

    fix
    gives everyone global variable (maybe?)

--- Collab/Scenes/coin.gd
+++ Collab/Scenes/coin.gd
@@ -4,5 +4,5 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	$"/root/GlobalData".money += 1
+	$"/root/GlobalVar".money += 1
 	queue_free()
--- Collab/Scenes/global_var.tscn.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-class_name GlobalV
-#Edit
-var playerHealth
-var money: int = 0
